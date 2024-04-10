dbbf435bc27add14d50cb43b308474d66d12678a
Roman <r.petliak12@gmail.com>
2024-04-10 04:07:15 +0000

    Added sendCode py func

--- server/server.py
+++ server/server.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
 from dotenv import load_dotenv
 import os
+import secrets
 
 load_dotenv()  # loads the .env file
 
@@ -14,8 +16,17 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Flask Mail config
+app.config["MAIL_SERVER"] = 'smtp.mailgun.org'
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = 'postmaster@sandbox16fc518c084b46f9b39de8bb09cfcf60.mailgun.org'
+app.config["MAIL_PASSWORD"] = 'c13c0ba71d9f4be2d68e46d3da84462a-4b670513-0c04e7a5'
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+mail = Mail(app)
 
 
 class User_Info(database.Model):
@@ -92,6 +103,46 @@ def login():
         return jsonify({"message": "Login successful"}), 200
     else:
         return jsonify({"message": "Invalid credentials"}), 401
+
+
+@app.route("/api/forgotPassword", methods=["GET", "POST"])
+def forgotPassword():
+    data = request.get_json()
+    # Todo
+    return data  # place holder
+
+
+@app.route("/api/sendCode", methods=["GET", "POST"])
+def sendCode():
+    data = request.get_json()
+    user = User_Info.query.filter_by(email=data.get("email")).first()
+
+    try:
+        if user and request.method == "POST":
+            # Generate a verification code
+            verification_code = secrets.token_urlsafe()
+
+            # Send the code to the users email
+            email = data.get("email")
+            # token = s.dumps(email, salt='email-confirm')
+
+            msg = Message(
+                "Confirm Email",
+                sender="No-Reply@PlanetExpress.project",
+                recipients=["r.petliak12@gmail.com"])
+
+            # link = url_for('confirm_email', token=token, _external=True)
+            msg.body = "Your link is (some code)"
+
+            mail.send(msg)
+            #
+            return jsonify({"message": "Code has been sent to the email."}), 200
+        else:
+            return jsonify({"message": "Invalid email, Please try again."}), 401
+    except Exception as e:
+        print(f"Error sending email: {e}")
+
+        return jsonify({"error": "Failed to send email", "details": str(e)}), 500
 
 
 if __name__ == "__main__":
