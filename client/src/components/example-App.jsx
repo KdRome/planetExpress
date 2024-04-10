4bb3ca9b78ea02f1963147b6ba38d5a6a845409f
arteeguz <arteeguz@gmail.com>
2024-04-10 18:48:28 -0400

    Pulled Main changes

---
+++ client/src/components/example-App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        Click on the Vite and React logos to learn more
+    </>
+  )
+}
+
+export default App
