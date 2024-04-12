cfa64e739124c786088b0335f433c663f6f07474
KdRome <r.petliak12@gmail.com>
2024-04-12 14:29:07 -0400

    Added email verif forms

--- client/src/SignUp.jsx
+++ client/src/SignUp.jsx
@@ -7,6 +7,8 @@ function SignUp() {
   const [password, setPassword] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [showVerificationInput, setShowVerificationInput] = useState(false);
+  const [code, setCode] = useState('');
   const [error, setError] = useState('');
   const [notif, setNotif] = useState('');
 
@@ -15,12 +17,14 @@ function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    setShowVerificationInput(true);
+
     // Simple front-end validation
     if(!email || !password || !firstName || !lastName) {
       setError("Please fill in all fields.");
       return;
     }
-
+    
     try {
       const response = await axios.post(`${apiUrl}create_user`, {
         email,
@@ -30,7 +34,7 @@ function SignUp() {
       });
 
       console.log("Account creation successful", response.data);
-      setNotif("Account creation successful, Please Login.");
+      setNotif("Please input the code from your email");
       //redirect the user here or next steps
     } catch (err) {
       if (err.response && err.response.status == 409){
@@ -41,6 +45,25 @@ function SignUp() {
         setError("Account creation failed. Please try again.");
         console.error(err);
       }
+    }
+  };
+
+  const handleEmailVerification = async (e) => {
+    e.preventDefault;
+
+    if(!code){
+      setError("Please input the code from email");
+      return;
+    }
+
+    try {
+      const response = await axios.post(`${apiUrl}emailVerification`, {
+        email,
+        code
+      });
+    } catch (err) {
+      setError("Code verificaiton failed. Please try again");
+      console.error(err);
     }
   };
   
@@ -89,7 +112,21 @@ function SignUp() {
               required
             />
           </div>
-          <button className="button" type="submit">Sign Up</button>
+          {!showVerificationInput ? (
+            <button className="button" type="submit">Sign Up</button>
+          ) : (
+            <><div className="formGroup">
+                <label>Code:</label>
+                <input
+                  className="input"
+                  type="text"
+                  value={code}
+                  onChange={(e) => setCode(e.target.value)}
+                  required />
+              </div>
+              <button className="button" onClick={handleEmailVerification}>Verify Code</button>
+            </>
+          )}
         </form>
         {notif && <p className="notifMessage">{notif}</p>}
         {error && <p className="errorMessage">{error}</p>}
