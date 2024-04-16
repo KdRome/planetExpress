42c958b35e4d8b36b44e44346db99b65bd171b8e
KdRome <r.petliak12@gmail.com>
2024-04-15 20:11:21 -0400

    fixed imports

--- server/emailRoutes.py
+++ server/emailRoutes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from databaseModels import User_Info, VerificationCode
 from server import mail
-from extensions import database
+from extensions import database, bcrypt
 from flask_mail import Message
 from datetime import datetime
 import secrets
--- server/userRoutes.py
+++ server/userRoutes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from databaseModels import User_Info
 from server import bcrypt
+from extensions import database
 
 def getDatabase():
     from server import database
