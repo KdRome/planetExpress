a0f04d3bdd9c9530ea04cf71dceac51f879a5edc
Roman <119768219+KdRome@users.noreply.github.com>
2024-04-27 02:34:13 -0400

    product Blueprint added

---
+++ server/routes/productRoutes.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, jsonify, request
+from models.databaseModels import Product
+from server import bcrypt
+from extensions.extensions import database
+
+productBP = Blueprint('productBP', __name__)
+
+@productBP.route("/api/products", methods=["GET"])
+def get_products():
+    all_product_data = Product.query.all()
+    return jsonify(
+        {
+            "products": [
+                {
+                    "id": product.product_id,
+                    "title": product.title,
+                    "category": product.category,
+                    "description": product.description,
+                    "image": product.image,
+                    "price": product.price,
+                    "rating": product.rating,
+                    "uri": product.uri,
+                }
+                for product in all_product_data
+            ]
+        }
+    )
+
+@productBP.route("/api/products/<int:product_id>", methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify(
+            {
+                "product": {
+                        "id": product.product_id,
+                        "title": product.title,
+                        "category": product.category,
+                        "description": product.description,
+                        "image": product.image,
+                        "price": product.price,
+                        "rating": product.rating,
+                        "uri": product.uri,
+                    }
+            }
+        )
+    else:
+        return jsonify({"error": "Product not found"}), 404
