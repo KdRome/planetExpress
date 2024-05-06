0ca1076d5db1da0472ffab8cdf14a1085badee2c
Roman <119768219+KdRome@users.noreply.github.com>
2024-05-06 00:08:40 -0400

    style changed

--- client/src/components/my_account/MyAccount.jsx
+++ client/src/components/my_account/MyAccount.jsx
@@ -17,15 +17,15 @@ const MyAccount = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div>
-        <h1 className="text-3xl font-semibold mb-8 text-center">My Account</h1>
-        <button
-          type="button"
-          className="washed-gray-bg hover:bg-gray-700 text-white font-bold py-2 px-4 mb-8 w-48 mt-8"
-          onClick={() => handleLogOut()}
-        >
-          Sign Out
-        </button>
+      <div className="flex items-center justify-center space-x-4 mb-8">
+          <h1 className="text-3xl font-semibold text-center">My Account</h1>
+          <button
+              type="button"
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 w-48"
+              onClick={() => handleLogOut()}
+          >
+              Sign Out
+          </button>
       </div>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
         <UserInfoForm />
